9d71b376a1d4d70372b15f8d28688b20ffa3a507
Vasudeo Gaichor <vasudeogaichor@gmail.com>
2025-02-24 00:12:52 +0530

    fixed undefined peerid

--- server.js
+++ server.js
@@ -31,143 +31,112 @@ app.get("/create-meeting", (req, res) => {
   res.json({ meetingId });
 });
 
-wss.on("connection", (socket) => {
-  socket.on("message", (message) => {
+function forwardSignal(data) {
+  // console.log('forwardSignal - ', data.type, data.targetPeerId)
+  const meeting = meetings[data.meetingId];
+  // console.log('meeting - ', meeting)
+  if (meeting) {
+    meeting.participants.forEach(peer => {
+      if (peer.peerId === data.targetPeerId) {
+        // console.log({
+        //   type: data.type,
+        //   peerId: peer.peerId
+        // })
+        peer.send(JSON.stringify({
+          type: data.type,
+          payload: data.payload, // Send the whole payload
+          peerId: data.peerId
+        }));
+      }
+    });
+  }
+}
+
+function leaveMeeting(socket, meetingId) {
+  const meeting = meetings[meetingId];
+  if (meeting) {
+    meeting.participants = meeting.participants.filter(p => p !== socket);
+
+    // Notify other participants about the leaving peer
+    meeting.participants.forEach(peer => {
+      peer.send(JSON.stringify({ type: 'participant-left', peerId: socket.peerId }));
+    });
+
+    if (meeting.participants.length === 0) {
+      delete meetings[meetingId]; // Clean up empty meetings
+    }
+  }
+  socket.close();
+}
+
+wss.on('connection', socket => {
+  socket.on('message', message => {
     const data = JSON.parse(message);
-    console.log("Received message:", data);
+    console.log('Received backend:', data);
 
     switch (data.type) {
-      case "create-meeting":
-        const meetingId = Date.now().toString();
-        meetings[meetingId] = { participants: [] };
-        socket.send(JSON.stringify({ type: "meeting-created", meetingId }));
-        break;
-
-      case "join-meeting":
-        if (!meetings[data.meetingId]) {
-          meetings[data.meetingId] = { participants: [] };
-        }
-
-        // Assign a unique ID to the participant
+      case 'join-meeting':
+        const meetingId = data.meetingId;
         const peerId = Math.random().toString(36).substr(2, 9);
         socket.peerId = peerId;
-        meetings[data.meetingId].participants.push(socket);
 
-        // Notify existing participants about the new participant
-        meetings[data.meetingId].participants.forEach((peer) => {
+        if (!meetings[meetingId]) {
+          meetings[meetingId] = { participants: [] };
+        }
+
+        meetings[meetingId].participants.push(socket);
+
+        // Notify existing participants
+        meetings[meetingId].participants.forEach(peer => {
           if (peer !== socket) {
-            peer.send(
-              JSON.stringify({ type: "new-participant", peerId: socket.peerId })
-            );
+            peer.send(JSON.stringify({ type: 'new-participant', peerId }));
           }
         });
 
-        // Notify the joining participant about existing peers
-        const existingPeers = meetings[data.meetingId].participants
-          .filter((peer) => peer !== socket)
-          .map((peer) => peer.peerId);
-
-        socket.send(
-          JSON.stringify({
-            type: "existing-participants",
-            peers: existingPeers,
-          })
-        );
-
-        console.log("Updated meetings:", meetings);
+        // Notify the new participant about existing ones
+        const existingPeers = meetings[meetingId].participants
+          .filter(p => p !== socket)
+          .map(p => p.peerId);
+        socket.send(JSON.stringify({ type: 'existing-participants', peers: existingPeers, currentPeerId: peerId }));
         break;
 
-      case "signal":
-        if (meetings[data.meetingId]) {
-          meetings[data.meetingId].participants.forEach((client) => {
-            if (client !== socket) {
-              client.send(JSON.stringify(data));
-            }
-          });
-        }
+      case 'offer':
+      case 'answer':
+        forwardSignal(data);
         break;
 
-      case "leave-meeting":
-        if (meetings[data.meetingId]) {
-          meetings[data.meetingId].participants = meetings[
-            data.meetingId
-          ].participants.filter((p) => p !== socket);
-          socket.close();
-        }
+      case 'candidate':
+        forwardSignal({
+          ...data,
+          payload: { // Recreate the candidate object with all required properties
+            candidate: data.payload.candidate,
+            sdpMid: data.payload.sdpMid,
+            sdpMLineIndex: data.payload.sdpMLineIndex,
+          }
+        });
+        break;
+
+      case 'leave-meeting':
+        leaveMeeting(socket, data.meetingId);
         break;
     }
   });
-  // wss.on("connection", (socket) => {
-  //   socket.on("message", (message) => {
-  //     const data = JSON.parse(message);
-  //     console.log("data - ", data);
 
-  //     switch (data.type) {
-  //       case "create-meeting":
-  //         const meetingId = Date.now().toString();
-  //         meetings[meetingId] = { participants: [] };
-  //         socket.send(JSON.stringify({ type: "meeting-created", meetingId }));
-  //         break;
-
-  //       case "join-meeting":
-  //         if (meetings[data.meetingId]) {
-  //           meetings[data.meetingId].participants.push(socket);
-  //           meetings[data.meetingId].participants.forEach((peer) => {
-  //             if (peer !== socket) {
-  //               peer.send(JSON.stringify({ type: "new-participant" }));
-  //             }
-  //           });
-  //         } else {
-  //           meetings[data.meetingId] = { participants: [socket] };
-  //         }
-  //         console.log("meetings = ", meetings);
-  //         break;
-
-  //       // case 'signal':
-  //       //     meetings[data.meetingId]?.participants.forEach(peer => {
-  //       //         if (peer !== socket) {
-  //       //             peer.send(JSON.stringify(data));
-  //       //         }
-  //       //     });
-  //       //     break;
-
-  //       case "signal":
-  //         if (meetings[data.meetingId]) {
-  //           meetings[data.meetingId].participants.forEach((client) => {
-  //             if (client !== socket) {
-  //               client.send(JSON.stringify(data));
-  //             }
-  //           });
-  //         }
-  //         break;
-
-  //       case "leave-meeting":
-  //         if (meetings[data.meetingId]) {
-  //           meetings[data.meetingId].participants = meetings[
-  //             data.meetingId
-  //           ].participants.filter((p) => p !== socket);
-  //           socket.close();
-  //         }
-  //         break;
-  //     }
-  //   });
-
-  //   socket.on("close", () => {
-  //     Object.keys(meetings).forEach((meetingId) => {
-  //       meetings[meetingId].participants = meetings[
-  //         meetingId
-  //       ].participants.filter((p) => p !== socket);
-  //     });
-  //   });
-
-  socket.on("close", () => {
-    Object.keys(meetings).forEach((meetingId) => {
-      meetings[meetingId].participants = meetings[
-        meetingId
-      ].participants.filter((p) => p !== socket);
+  socket.on('close', () => {
+    Object.keys(meetings).forEach(meetingId => {
+      leaveMeeting(socket, meetingId);
     });
   });
+
+  socket.on('error', (error) => {
+    console.error("Socket error:", error);
+    Object.keys(meetings).forEach(meetingId => {
+      leaveMeeting(socket, meetingId);
+    });
+  });
+
 });
+
 
 server.listen(3000, () => {
   console.log("Server running on http://localhost:3000");
